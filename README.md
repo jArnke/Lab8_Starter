645465ca9d3bb18dd58024a8121968b1643aa4d4
jAnke <jyenney@ucsd.edu>
2022-11-18 12:35:15 -0800

    unit tests

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Lab 8 - Starter
+
+1) I think it is important to set it up in a github action that runs whenever code is pushed to ensure that any changes saved to the repository work as expected, however it is also useful to run them in your local environment so that you aren't needlessly committing work that doesn't pass test cases.
+
+2) E2E testing you write a test case which interacts with the DOM, 
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -2,4 +2,38 @@
 
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
+test("Check isPhoneNumber Function",  () => {
+    expect(functions.isPhoneNumber("(123)456-7890")).toBe(true);
+    expect(functions.isPhoneNumber()).toBe(false);
+    expect(functions.isPhoneNumber("(111)11111111")).toBe(false);
+    expect(functions.isPhoneNumber("(111)111-1111")).toBe(true);
+});
+
+test("Check isEmail", () => {
+    expect(functions.isEmail("test@gmail.com")).toBe(true);
+    expect(functions.isEmail("test@gmail")).toBe(false);
+    expect(functions.isEmail("tl.com")).toBe(false);
+    expect(functions.isEmail("t@goo.org")).toBe(true);
+});
+
+test("Check isStrongPassword", () => {
+   expect(functions.isStrongPassword("fart")).toBe(true);
+   expect(functions.isStrongPassword("art")).toBe(false);
+   expect(functions.isStrongPassword("f8*")).toBe(false);
+   expect(functions.isStrongPassword("f123")).toBe(true); 
+});
+
+test("Check isDate", () => {
+    expect(functions.isDate("10/20/2020")).toBe(true);
+    expect(functions.isDate("1/2/22")).toBe(false);
+    expect(functions.isDate("1/2/1920")).toBe(true);
+    expect(functions.isDate("10/200/2020")).toBe(false);
+});
+
+test("Check isHexColor", () => {
+    expect(functions.isHexColor("203Faa")).toBe(true);
+    expect(functions.isHexColor("FFF")).toBe(true);
+    expect(functions.isHexColor("F@T")).toBe(false);
+    expect(functions.isHexColor("2Faa")).toBe(false);
+});
 // TODO - Part 2
